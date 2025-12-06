14b73759c816db434383e2fca453ffd74f022109
AjitDM <ajitmusane2608@gmail.com>
2025-12-06 14:19:46 +0530

    corrected the test case

--- test_predict.py
+++ test_predict.py
@@ -22,4 +22,4 @@ def test_predict(client):
     }
     resp = client.post('/predict', json = test_data)
     assert resp.status_code == 200
-    assert resp.json == {"loan_approval_status" : "Approved"}
+    assert resp.json == {"loan_approval_status" : "Rejected"}
